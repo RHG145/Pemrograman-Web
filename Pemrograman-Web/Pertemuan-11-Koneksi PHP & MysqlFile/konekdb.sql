d382e861576466bcab3263235c6f7ee02cb31fd4
RHG <86063265+RHG145@users.noreply.github.com>
2022-06-25 11:35:13 +0700

    Update konekdb.sql

--- Pemrograman-Web/Pertemuan-11-Koneksi PHP & MysqlFile/konekdb.sql
+++ Pemrograman-Web/Pertemuan-11-Koneksi PHP & MysqlFile/konekdb.sql
@@ -1,26 +1,26 @@
 --
--- Database: 'konekdb'
+-- Database: `konekdb`
 --
 
 -- -----------------------------------------------------------------
 
 --
--- Table structure for table 'user'
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS 'user' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'nama' varchar(50) NOT NULL,
-  'alamat' text NOT NULL,
-  'pekerjaan' varchar(50) NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `pekerjaan` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=iatini AUTO_INCREMENT=64 ;
 
 --
--- Dumping data for table 'user'
+-- Dumping data for table `user`
 --
 
-INSERT INTO 'user' ('id', 'nama', 'alamat', 'pekerjaan') VALUES
+INSERT INTO `user` (`id`, `nama`, `alamat`, `pekerjaan`) VALUES
 (1, 'Andi', 'Surabaya', 'web programmer'),
 (2, 'Santoso', 'Jakarta', 'web Designer'),
 (6, 'Samsul', 'Sumedang', 'Pegawai');
