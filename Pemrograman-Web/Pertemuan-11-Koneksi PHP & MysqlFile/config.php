ba0884590260b78da22abfcbeb01b60296bfa167
RHG <86063265+RHG145@users.noreply.github.com>
2022-06-25 11:52:39 +0700

    Update config.php

--- Pemrograman-Web/Pertemuan-11-Koneksi PHP & MysqlFile/config.php
+++ Pemrograman-Web/Pertemuan-11-Koneksi PHP & MysqlFile/config.php
@@ -8,7 +8,7 @@ $databasePassword = '';
 $mysqli = mysqli_connect ($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 
 if (!$mysqli) {
-    die("Koneksi gagal: " .mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 echo "Koneksi berhasil";
 mysqli_close ($mysqli);
