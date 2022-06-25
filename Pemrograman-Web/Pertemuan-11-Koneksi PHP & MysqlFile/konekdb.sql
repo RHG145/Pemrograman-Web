726028dd79b505e3aa8d8002e6ea37158a58abb9
RHG <86063265+RHG145@users.noreply.github.com>
2022-06-25 12:03:38 +0700

    Update konekdb.sql

--- Pemrograman-Web/Pertemuan-11-Koneksi PHP & MysqlFile/konekdb.sql
+++ Pemrograman-Web/Pertemuan-11-Koneksi PHP & MysqlFile/konekdb.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `alamat` text NOT NULL,
   `pekerjaan` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=iatin1 AUTO_INCREMENT=64 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
 
 --
 -- Dumping data for table `user`
