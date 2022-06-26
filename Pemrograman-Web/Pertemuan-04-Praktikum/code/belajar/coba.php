92074229af9bbb9ee2c7e2bd296900aa5e783851
RHG <86063265+RHG145@users.noreply.github.com>
2022-06-26 12:25:15 +0700

    Delete Pemrograman-Web directory

--- Pemrograman-Web/Pertemuan-04-Praktikum/code/belajar/coba.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Saya sedang belajar PHP di kelas Pemrograman Web Universitas Paramadina";
-echo "<br>";
-echo "Rahagi Kautsar 120103043";
-echo "<br>";
-echo "Hello World...";
-?>
--- Pemrograman-Web/Pertemuan-13-Koneksi DB PHP Session LoginFile/xampp/htdocs/pertemuan13/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$koneksi = mysqli_connect
-	("localhost","root","","dbminggu13");
-	
-	if (mysqli_connect_error()){
-		echo "Koneksi database gagal :
-		" . mysqli_connect_error();
-	}
-?>
